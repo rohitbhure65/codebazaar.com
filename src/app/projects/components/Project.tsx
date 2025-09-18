e34f22b44652bfdda2fc263b51e9f4bcf189ae4a
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-09-18 10:49:37 +0530

    working

--- src/app/projects/components/Project.tsx
+++ src/app/projects/components/Project.tsx
@@ -44,9 +44,9 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                 month: "long",
                 day: "numeric",
               })}
-            </span> 
+            </span>
             <span>
-             | Updated on
+              | Updated on
               {new Date(project.updatedAt).toLocaleDateString("en-US", {
                 year: "numeric",
                 month: "long",
@@ -103,7 +103,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                   />
                 </svg>
                 <span className="text-gray-600">
-                  Downloads:{" "}
+                  Downloads:
                   <span className="font-medium text-gray-800">{project.downloads || 0}</span>
                 </span>
               </div>
@@ -123,7 +123,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                     />
                   </svg>
                   <span className="text-gray-600">
-                    Demo:{" "}
+                    Demo:
                     <a
                       href={project.demoUrl}
                       target="_blank"
@@ -141,7 +141,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                     <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                   </svg>
                   <span className="text-gray-600">
-                    Repository:{" "}
+                    Repository:
                     <a
                       href={project.repositoryUrl}
                       target="_blank"
@@ -168,7 +168,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                   />
                 </svg>
                 <span className="text-gray-600">
-                  Resell Allowed:{" "}
+                  Resell Allowed:
                   <span
                     className={`font-medium ${project.isResellAllowed ? "text-green-600" : "text-red-600"
                       }`}
@@ -192,7 +192,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
                   />
                 </svg>
                 <span className="text-gray-600">
-                  Approved:{" "}
+                  Approved:
                   <span
                     className={`font-medium ${project.isApproved ? "text-green-600" : "text-red-600"
                       }`}
@@ -207,7 +207,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
           <p className="text-gray-700">
             {project.tags && project.tags.length > 0 ? (
               <span className="flex flex-wrap gap-2">
-                <TextSnippetRoundedIcon /> <span className="text-sm font-bold">Tags</span>:{" "}
+                <TextSnippetRoundedIcon /> <span className="text-sm font-bold">Tags</span>:
                 {project.tags.flatMap((tag, index) =>
                   tag.split(" ").map((word, wordIndex) => (
                     <Badge key={`${index}-${wordIndex}`} variant="outline">
@@ -223,7 +223,7 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
           <p className="text-gray-700">
             {project.techStack && project.techStack.length > 0 ? (
               <span className="flex flex-wrap gap-2">
-                <CodeRoundedIcon /> <span className="text-sm font-bold">Tech Stack</span>:{" "}
+                <CodeRoundedIcon /> <span className="text-sm font-bold">Tech Stack</span>:
                 {project.techStack.flatMap((techStack, index) =>
                   techStack.split(" ").map((word, wordIndex) => (
                     <Badge key={`${index}-${wordIndex}`} variant="outline">
