4f59f8aafce5bc8672113b50ac6808d380008d53
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-09-18 20:22:04 +0530

    working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,46 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "CodeBazaar.com",
   description: "Cobazaar.com online market place for developers",
+  keywords: [
+    "CodeBazaar",
+    "Code Bazaar",
+    "CodeBazaar.com",
+    "Code Bazaar.com",
+    "Code Marketplace",
+    "Rohit Bhure Code Bazaar",
+    "Rohit Bhure CodeBazaar.com"],
+  robots: 'index, follow',
+  openGraph: {
+    title: 'CodeBazaar.com',
+    description: 'Cobazaar.com online market place for developers',
+    images: [
+      {
+        url: 'https://codebazaar.com/og-image.png',
+        width: 800,
+        height: 600,
+        alt: 'CodeBazaar.com',
+      },
+    ],
+    type: 'website',
+    url: 'https://codebazaar.com',
+    siteName: 'CodeBazaar',
+    countryName: 'India',
+    locale: 'en_IN'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CodeBazaar.com',
+    description: 'Cobazaar.com online market place for developers',
+    images: [
+      {
+        url: 'https://codebazaar.com/og-image.png',
+        width: 800,
+        height: 600,
+        alt: 'CodeBazaar.com',
+      },
+    ],
+  },
+  alternates: { canonical: 'https://codebazaar.com' },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
