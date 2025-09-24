97e010744af296ab04db088e4337c71a2aabae91
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-09-24 09:22:07 +0530

    w

--- src/app/projects/components/Project.tsx
+++ src/app/projects/components/Project.tsx
@@ -432,31 +432,45 @@ export const Project = ({ projectSlug }: { projectSlug: string }) => {
         </div>
       )}
 
-      {/* {project.videoUrl && (
+      {project.videoUrl && (
         <div className="relative w-full pb-[56.25%] mt-5 h-0 overflow-hidden rounded-xl shadow-md">
           <iframe
-            src={project.videoUrl}
+            src={`https://www.youtube.com/embed/${project.videoUrl}`}
             title={project.title}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             className="absolute top-0 left-0 w-full h-full"
           ></iframe>
-        </div>
-      )} */}
+          {/* <iframe
+            className="w-full h-full absolute"
+            src={String(project.videoUrl)}
+            title="YouTube video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe> */}
 
-      {project.description && (
-        <div className="bg-white rounded-lg shadow-md mt-5 p-6 border border-gray-200">
-          <h3 className="text-lg font-semibold text-gray-800 mb-4"><ArticleRoundedIcon /> Description</h3>
-          <p className="mb-4">{project.description}</p>
         </div>
-      )}
+      )
+      }
 
-      {project.requirements && (
-        <div className="bg-white rounded-lg shadow-md mt-5 p-6 border border-gray-200">
-          <h3 className="text-lg font-semibold text-gray-800 mb-4"><ArticleRoundedIcon /> requirements</h3>
-          <p className="mb-4">{project.requirements}</p>
-        </div>
-      )}
+      {
+        project.description && (
+          <div className="bg-white rounded-lg shadow-md mt-5 p-6 border border-gray-200">
+            <h3 className="text-lg font-semibold text-gray-800 mb-4"><ArticleRoundedIcon /> Description</h3>
+            <p className="mb-4">{project.description}</p>
+          </div>
+        )
+      }
+
+      {
+        project.requirements && (
+          <div className="bg-white rounded-lg shadow-md mt-5 p-6 border border-gray-200">
+            <h3 className="text-lg font-semibold text-gray-800 mb-4"><ArticleRoundedIcon /> requirements</h3>
+            <p className="mb-4">{project.requirements}</p>
+          </div>
+        )
+      }
 
       <ProfileCard />
 
